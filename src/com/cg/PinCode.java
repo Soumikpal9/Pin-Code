3e4df84ed2f79a066bbd800aa3a07b8081bb8e70
Soumikpal9 <soumikpal1997@gmail.com>
2020-09-28 22:59:28 +0530

    Added: Welcome Message

---
+++ src/com/cg/PinCode.java
@@ -0,0 +1,9 @@
+package com.cg;
+import java.util.*;
+import java.util.regex.*;
+
+public class PinCode {
+	public static void main(String[] args) {
+		System.out.println("Welcome to pincode Problem!!!");
+	}
+}
